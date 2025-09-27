666029f30ff7c545724496ac2e28c2070cf4e350
dubeydiwakar07 <dubeydiwakar07@gmail.com>
2025-09-27 21:15:41 +0530

    Correcting the bucket name

--- my-cdk-project/lib/my-cdk-project-stack.ts
+++ my-cdk-project/lib/my-cdk-project-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as path from 'path';
 
 export class MyCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'Samplebucket', {
-      bucketName:"diw-test-bucket1",
+    const bucket = new s3.Bucket(this, 'Samplebucket', {
+      bucketName: "diw-test-bucket1",
       versioned: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // Only for dev/test
-      //autoDeleteObjects: true, // Only for dev/test
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      //autoDeleteObjects: true,
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployFolders', {
+      destinationBucket: bucket,
+      sources: [s3deploy.Source.asset(path.join(__dirname, '../s3-assets'))],
     });
   }
 }
